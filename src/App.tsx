47e3bd44544cfae0b9c4723c2daa9a2ff4d827ca
rsk7 <ravikrishnasingh@gmail.com>
2021-12-02 17:55:24 -0500

    app

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,24 @@ function App() {
   const start = 500;
   const width = 200;
 
+  const [matrix, setMatrix] = useState([1, 0, 0, 1, 0, 0]);
+
+  const test = () => {
+    console.log("test called");
+    const newMatrix = [...matrix];
+    newMatrix[4] += 5;
+    // newMatrix[5] += 5;
+    setMatrix(newMatrix);
+  };
+
+  const [mouseDownLocation, setMouseDownLocation] = useState({});
+
   return (
     <div className="App">
-      <svg>
-        <g id="matrix-group" transform="matrix(1 0 0 1 0 0)">
+      <svg
+        onMouseDown={(e) => setMouseDownLocation({ x: e.pageX, y: e.pageY })}
+      >
+        <g id="matrix-group" transform={`matrix(${matrix.join(" ")})`}>
           {getActors().map((a, index) => (
             <Lifeline x={start + index * width} y={200} name={a} />
           ))}
