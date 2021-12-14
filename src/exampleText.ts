5be19dfe2207cfa5e7b104af88d0f79098c9bc28
rsk7 <ravikrishnasingh@gmail.com>
2021-12-14 12:15:23 -0500

    change example

--- src/exampleText.ts
+++ src/exampleText.ts
@@ -3,18 +3,18 @@ export const exampleText = `SEQUENCE DIAGRAMS
 
 This is a tool that draws simple sequence diagrams. The diagrams are described in text using a simple language. To create a diagram start by defining some "actors" and some interactions between them.
 
-Remove the "#" character to start drawing.
+actors: user, computer, server
 
-#actors: user, computer, server
-#
-#user -- opens browser --> computer
-#computer -- sends request --> server
-#server -- sends response --> computer
-#computer -- shows page --> user
+user -- opens browser --> computer
+computer -- sends request --> server
+server -- sends response --> computer
+computer -- shows page --> user
 
 The diagram is being drawn in an SVG element. You can move it around by dragging it. Scroll up or down to zoom. This text box can also be moved using the move button on the top right and resized using the handle on the bottom right corner.
 
 Additional actors can be specified anywhere in the text. The actors description must always be in a new line. Spacing between actors can be configured using special syntax. For example: db|sp(x) will space out the next actor from db by x units. Try different values in the line below.
+
+Remove the "#" character to start drawing.
 
 #actors: db|sp(2), data warehouse
 #
